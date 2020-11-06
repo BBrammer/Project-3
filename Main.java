40b02740f9d266bee5a1a93f5224a04f8e57ef38
BBrammer <brandon.brammer@patelhs.org>
2020-11-06 18:45:36 +0000

    I added a scanner

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int[ ] seats = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+    String[] names = new String[15];
+
+    for (int i = 0; i < names.length; i++)
+
+    Scanner names = new Scanner(System.in);
+
+    System.out.println(names);
   }
 }
